971785d838d4d0ce56fb51851da797970f4ba824
Wang Hailong <2905219543@qq.com>
2022-12-05 09:19:13 +0800

    增加了commits和releases的部分，但是releases的仍然由一定的bug还不完全，其中Time.txt文件不是资源仅仅是中间为了处理某些数据保存的中间数据

---
+++ untitled/src/main/java/commits.java
@@ -0,0 +1,114 @@
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class commits {
+  public static void main(String[] args) throws IOException, ParseException {
+    int m = 2;
+    List<LinkedHashMap<String, Object>> dataList = new ArrayList<>();
+    String s = "https://api.github.com/repos/alibaba/fastjson/commits?per_page=100";
+    URL url = new URL(s);
+    PrintWriter out = new PrintWriter("commits.txt");
+    PrintWriter out2 = new PrintWriter("Time.txt");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+    connection.connect();
+    int[] d = new int[8];
+    while (m != 45) {
+      StringBuilder j = new StringBuilder();
+      Scanner in = new Scanner(connection.getInputStream());
+      while (in.hasNext()) {
+        j.append(in.next());
+      }
+      getCommitList(out2, d, dataList, j.toString());
+      s = "https://api.github.com/repos/alibaba/fastjson/commits?per_page=100&page="+m;
+      url = new URL(s);
+      connection = (HttpURLConnection) url.openConnection();
+      connection.setRequestMethod("GET");
+      connection.connect();
+      m++;
+    }
+    out.println("Total");
+    out.println("--------------");
+    out.println("Morning: "+d[0]);
+    out.println("Noon: "+d[1]);
+    out.println("Afternoon: "+d[2]);
+    out.println("Evening: "+d[3]);
+    out.println("Night: "+d[4]);
+    out.println("--------------");
+    out.println("Weekday: "+d[5]);
+    out.println("Weekend: "+d[6]);
+    for (int i = 0; i < dataList.size(); i++) {
+      out.println(dataList.get(i));
+    }
+    out.close();
+    out2.close();
+  }
+  public static void getCommitList(PrintWriter out, int[] d, List<LinkedHashMap<String, Object>> datalist, String s) throws ParseException {
+    JSONArray commits = JSONArray.parseArray(s);
+    for (int i = 0; i < commits.size(); i++) {
+      JSONObject js  = commits.getJSONObject(i);
+      JSONObject commit = js.getJSONObject("commit");
+      JSONObject author = commit.getJSONObject("author");
+      String name = author.get("name").toString();
+      String time = author.get("date").toString();
+      String Time1 = time.replace("Z", " UTC");
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss Z");
+      SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Date timeEnd = format.parse(Time1);
+      System.out.println(Time1);
+      String Time2 = format1.format(timeEnd);
+      Date date = format1.parse(Time2);
+      String t1 = Time2.replace(":", "");
+      String t2 = t1.replace("-", "");
+      String t3 = t2.replace(" ","");
+      out.println(t3);
+      System.out.println(Time2);
+      String hour = Time2.substring(11, 13);
+      int iHour = Integer.parseInt(hour);
+      String day;
+      String dd;
+      if (iHour >= 6 && iHour <= 11) {
+        day = "morning";
+        d[0]++;
+      } else if (iHour > 11 && iHour <= 13) {
+        day = "noon";
+        d[1]++;
+      } else if (iHour > 13 && iHour <= 18) {
+        day = "afternoon";
+        d[2]++;
+      } else if (iHour > 18 && iHour <= 22) {
+        day = "evening";
+        d[3]++;
+      } else {
+        day = "night";
+        d[4]++;
+      }
+      Calendar calendar = Calendar.getInstance();
+      calendar.setTime(date);
+      int weekday = calendar.get(Calendar.DAY_OF_WEEK);
+      if (weekday >= 1 && weekday <= 5) {
+        dd = "Weekday";
+        d[5]++;
+      } else {
+        dd ="Weekend";
+        d[6]++;
+      }
+      String t = dd+" "+day;
+      LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+      map.put("Name", name);
+      map.put("Date", Time2);
+      map.put("Time", t);
+      datalist.add(map);
+    }
+  }
+}
--- untitled/src/main/java/release.java
+++ untitled/src/main/java/release.java
@@ -1,2 +1,107 @@
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.camel.util.json.JsonArray;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class release {
+  public static void main(String[] args) throws IOException, ParseException {
+    String s = "https://api.github.com/repos/alibaba/fastjson/releases?per_page=100";
+    URL url = new URL(s);
+    Scanner in1 = new Scanner(new File("Time.txt"));
+    PrintWriter out = new PrintWriter("release.txt");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("GET");
+    connection.connect();
+    Scanner in = new Scanner(connection.getInputStream());
+    StringBuilder js = new StringBuilder();
+    while(in.hasNext()) {
+      js.append(in.next());
+    }
+    ArrayList<Long> time = new ArrayList<>();
+    while (in1.hasNext()) {
+      time.add(in1.nextLong());
+    }
+    Collections.reverse(time);
+    String s2 = js.toString();
+    List<LinkedHashMap<String, Object>> dateList = new ArrayList<>();
+    getReleasesJs(dateList, s2, time);
+    out.println("Total Releases: "+dateList.size());
+    for (int i = 0; i  < dateList.size(); i++) {
+      out.println(dateList.get(i));
+    }
+    out.close();
+  }
+  public static void getReleasesJs (List<LinkedHashMap<String, Object>> dateList, String s, ArrayList<Long> time) throws ParseException {
+    JSONArray release = JSONArray.parseArray(s);
+    for (int i = 0 ; i < release.size(); i++) {
+      LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+      JSONObject re = release.getJSONObject(i);
+      String tag = re.get("tag_name").toString();
+      String cr = re.get("created_at").toString();
+      String en = re.get("published_at").toString();
+      String Time1 = en.replace("Z", " UTC");
+      SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss Z");
+      SimpleDateFormat format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Date timeEnd = format.parse(Time1);
+      String Time2 = format1.format(timeEnd);
+      String Timec = cr.replace("Z", " UTC");
+      System.out.println(cr);
+      System.out.println(en);
+      SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss Z");
+      SimpleDateFormat format3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+      Date timeSt = format2.parse(Timec);
+      String TimeSt = format3.format(timeSt);
+      String t1 = Time2.replace(":", "");
+      String t2 = t1.replace("-", "");
+      String t3 = t2.replace(" ","");
+      long end = Long.parseLong(t3);
+      String t4 = TimeSt.replace(":", "");
+      String t5 = t4.replace("-", "");
+      String t6 = t5.replace(" ","");
+      long begin = Long.parseLong(t6);
+      int totalCommit = getCommits(begin, end, time);
+      map.put("Tag_name", tag);
+      map.put("Published_at", Time2);
+      map.put("NewCommits", totalCommit);
+      dateList.add(map);
+    }
+  }
+  public static int getCommits (long begin, long end, ArrayList<Long> time) {
+    int blue1 = -1;
+    System.out.println(begin);
+    System.out.println(end);
+    int red1 = time.size();
+    int mid1;
+    while (blue1 + 1 != red1) {
+       mid1 = (blue1 + red1) / 2;
+      if (time.get(mid1) < begin) {
+        blue1 = mid1;
+      } else {
+        red1 = mid1;
+      }
+    }
+    int blue2 = -1;
+    int red2 = time.size();
+    int mid2;
+    while (blue2 + 1 != red2) {
+      mid2 = (blue2 + red2) / 2;
+      if (time.get(mid2) <= end) {
+        blue2 = mid2;
+      } else {
+        red2 = mid2;
+      }
+    }
+    return blue2 - red1 + 1;
+  }
 }

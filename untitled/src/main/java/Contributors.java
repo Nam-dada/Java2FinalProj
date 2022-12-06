49121f0ebdafd05ce466a258d27dce6277ecb120
Wang Hailong <2905219543@qq.com>
2022-12-06 16:20:22 +0800

    新增了anonymous的用户 login为anonymous

--- untitled/src/main/java/Contributors.java
+++ untitled/src/main/java/Contributors.java
@@ -9,51 +9,55 @@ import java.util.*;
 
 public class Contributors {
   public static void main(String[] args) throws IOException {
-    StringBuilder j = new StringBuilder();
+    List<LinkedHashMap<String, Object>> a = new ArrayList<>();
     int m = 2;
-    String s = "https://api.github.com/repos/alibaba/fastjson/contributors?per_page=100";
+    String s = "https://api.github.com/repos/alibaba/fastjson/contributors?per_page=100&anon=1";
     URL url = new URL(s);
     PrintWriter out = new PrintWriter("Contributors.txt");
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
     connection.setRequestMethod("GET");
     connection.connect();
-    while (m != 4) {
+    while (m != 6) {
+      StringBuilder j = new StringBuilder();
     Scanner in = new Scanner(connection.getInputStream());
     while (in.hasNext()) {
       j.append(in.next());
     }
-      s = "https://api.github.com/repos/alibaba/fastjson/contributors?per_page=100&page="+m;
+      getJsonList(a, j.toString());
+      s = "https://api.github.com/repos/alibaba/fastjson/contributors?per_page=100&page="+m+"&anon=1";
       url = new URL(s);
       connection = (HttpURLConnection) url.openConnection();
       connection.setRequestMethod("GET");
       connection.connect();
       m++;
     }
-    String temp = j.toString();
-    String temp2 = temp.replace("[", "");
-    String temp3 = temp2.replace("]", "");
-    String js = "["+temp3+"]";
-     List<LinkedHashMap<String, Object>> a = getJsonList(js);
+    int total = 0;
       Collections.sort(a, (m1, m2) -> Integer.compare(Integer.parseInt(m2.get("contributions").toString()), Integer.parseInt(m1.get("contributions").toString())));
     for (int i = 0; i < a.size(); i++) {
       out.println(a.get(i));
+      total += Integer.parseInt(a.get(i).get("contributions").toString());
     }
-    
+    out.println(total);
       out.close();
   
   }
-  public static List<LinkedHashMap<String, Object>> getJsonList(String s) {
-    List<LinkedHashMap<String, Object>> dataList = new ArrayList<>();
+  public static void getJsonList(List<LinkedHashMap<String, Object>> dataList, String s) {
     JSONArray contributors =  JSONArray.parseArray(s);
     for (int i = 0; i < contributors.size(); i++) {
       JSONObject j = (JSONObject) contributors.get(i);
-      String login = j.get("login").toString();
-      String contributions = j.get("contributions").toString();
-      LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-      map.put("login", login);
-      map.put("contributions", contributions);
-      dataList.add(map);
+      if (j.get("type").toString().equals("Anonymous")) {
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("login", "Anonymous");
+        map.put("contributions", j.get("contributions").toString());
+        dataList.add(map);
+      } else {
+        String login = j.get("login").toString();
+        String contributions = j.get("contributions").toString();
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("login", login);
+        map.put("contributions", contributions);
+        dataList.add(map);
+      }
     }
-    return dataList;
   }
 }

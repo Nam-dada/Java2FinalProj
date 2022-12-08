5691aa6b94fde576aae27df8d95f077e322e4930
YN <1109225291@qq.com>
2022-12-09 00:23:49 +0800

    数据注入成功

--- java2DataPresent/data.py
+++ java2DataPresent/data.py
@@ -6,6 +6,8 @@
 # @Describe:
 
 import json
+import numpy as np
+
 
 class SourceDataDemo:
 
@@ -92,23 +94,132 @@ class SourceDataDemo:
             ]
         }
         self.echart6_data = {
-            'title': '一线城市情况',
+            'title': 'Empty',
             'data': [
-                {"name": "浙江", "value": 80, "value2": 20, "color": "01", "radius": ['59%', '70%']},
-                {"name": "上海", "value": 70, "value2": 30, "color": "02", "radius": ['49%', '60%']},
-                {"name": "广东", "value": 65, "value2": 35, "color": "03", "radius": ['39%', '50%']},
-                {"name": "北京", "value": 60, "value2": 40, "color": "04", "radius": ['29%', '40%']},
-                {"name": "深圳", "value": 50, "value2": 50, "color": "05", "radius": ['20%', '30%']},
+                {"name": "var1", "value": 80, "value2": 20, "color": "01", "radius": ['59%', '70%']},
+                {"name": "var2", "value": 70, "value2": 30, "color": "02", "radius": ['49%', '60%']},
+                {"name": "var3", "value": 65, "value2": 35, "color": "03", "radius": ['39%', '50%']},
+                {"name": "var4", "value": 60, "value2": 40, "color": "04", "radius": ['29%', '40%']},
+                {"name": "var5", "value": 50, "value2": 50, "color": "05", "radius": ['20%', '30%']},
             ]
         }
         self.map_1_data = {
             'symbolSize': 100,
+            'data': []
+        }
+
+    def loadfile(self, name):
+        with open(name, 'r', encoding='utf-8') as f:
+            contributors, issues, releases, commits = f.read().split('-spy raye ace-')
+        contributors = contributors.split('\n')[:-1]
+        issues = issues.split('\n')[1:-1]
+        releases = releases.split('\n')[1:-1]
+        commits = commits.split('\n')[1:-1]
+        self.title = 'Repo name:' + name.replace('.txt', '') + ' Data date:' + contributors[0].split(':')[1]
+        self.counter = {'name': 'Count of developers', 'value': int(contributors[-1])}
+        self.counter2 = {'name': 'Count of releases', 'value': int(releases[0].split(':')[-1])}
+
+        self.echart1_data = {
+            'title': 'Top 6 commits\' developers',
+            'data': list()
+        }
+        for i in range(1, 7):
+            temp_contri = contributors[i].split(',')
+            tmp = {'name': str(temp_contri[0].split('=')[1]), 'value': int(temp_contri[1].split('=')[1][:-1])}
+            self.echart1_data['data'].append(tmp)
+
+        self.echart2_data = {
+            'title': 'Typical handling of issue resolution time',
+            'data': []
+        }
+
+        total_time = []
+        op_cnt = int(issues[0].split(':')[1])
+        close_cnt = int(issues[1].split(':')[1])
+        issues = issues[2:]
+        for isu in issues:
+            if isu[-3:-1] != 'No':
+                temp_idx = isu.rfind('=')
+                isu_time = isu[temp_idx + 1: -1].split('Minutes')[0].replace(' ', '').split('Hours')
+                temp_time = int(isu_time[0].split('Days')[0]) * 1440 + int(isu_time[0].split('Days')[1]) * 60 + int(
+                    isu_time[1])
+                total_time.append(temp_time)
+        self.echart2_data['data'].append({'name': 'avg time', 'value': int(np.mean(total_time))})
+        self.echart2_data['data'].append(
+            {'name': 'range of time', 'value': int(np.max(total_time) - np.min(total_time))})
+        self.echart2_data['data'].append({'name': 'var of time', 'value': int(np.var(total_time))})
+
+        self.echarts3_1_data = {
+            'title': 'Count of issues',
             'data': [
-                {'name': '海门', 'value': 239},
-                {'name': '鄂尔多斯', 'value': 231},
-                {'name': '招远', 'value': 203},
+                {"name": "open", "value": op_cnt},
+                {"name": "closed", "value": close_cnt},
             ]
         }
+
+        # 搁置 3_2 和 3_3
+
+        self.echart4_data = {
+            'title': 'Time distribution of commits',
+            'data': [],
+            'xAxis': ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '11', '12', '13', '14', '15', '16',
+                      '17', '18', '19', '20', '21', '22', '23'],
+        }
+
+        weekday_time = [0 for x in range(0, 24)]
+        weekend_time = [0 for x in range(0, 24)]
+        Morning = int(commits[2].split(':')[1])  # (6-11]
+        Noon = int(commits[3].split(':')[1])  # (11-13]
+        Afternoon = int(commits[4].split(':')[1])  # (13-18]
+        Evening = int(commits[5].split(':')[1])  # (18-22]
+        Night = int(commits[6].split(':')[1])  # (22-6]
+        Weekday = int(commits[8].split(':')[1])
+        Weekend = int(commits[9].split(':')[1])
+
+        self.echarts3_2_data = {
+            'title': '日时段分布',
+            'data': [
+                {"name": "Morning", "value": int(Morning)},
+                {"name": "Noon", "value": int(Noon)},
+                {"name": "Afternoon", "value": int(Afternoon)},
+                {"name": "Evening", "value": int(Evening)},
+                {"name": "Night", "value": int(Night)},
+            ]
+        }
+
+        self.echarts3_3_data = {
+            'title': '周时段分布',
+            'data': [
+                {"name": "Weekday", "value": int(Weekday)},
+                {"name": "Weekend", "value": int(Weekend)},
+            ]
+        }
+
+        commits = commits[10:]
+        for com in commits:
+            c_time, c_type = com.split(' ')[2:-1]
+            hour, _, _ = c_time.split(':')
+            if c_type.split('=')[1] == 'Weekday':
+                weekday_time[int(hour)] += 1
+            else:
+                weekend_time[int(hour)] += 1
+        self.echart4_data['data'].append({'name': 'Cnt of commits in weekday', 'value': weekday_time})
+        self.echart4_data['data'].append({'name': 'Cnt of commits in weekend', 'value': weekend_time})
+
+        self.echart5_data = {
+            'title': 'Commits\' diff between releases',
+            'data': []
+        }
+
+        releases = releases[1:]
+        for i in range(0, 6):
+            rel = releases[i]
+            l = rel.find('=')
+            r = rel.find(',')
+            r_name = rel[l + 1: r]
+            rr = rel.rfind('=')
+            count = rel[rr + 1: -1]
+            self.echart5_data['data'].append({'name': r_name, 'value': int(count)})
 
     @property
     def echart1(self):
@@ -209,7 +320,8 @@ class SourceData(SourceDataDemo):
         按照 SourceDataDemo 的格式覆盖数据即可
         """
         super().__init__()
-        self.title = '大数据可视化展板通用模板'
+        self.loadfile('fastjson.txt')
+
 
 class CorpData(SourceDataDemo):
 
@@ -232,6 +344,7 @@ class CorpData(SourceDataDemo):
         self.echart5_data = data.get('echart5_data')
         self.echart6_data = data.get('echart6_data')
         self.map_1_data = data.get('map_1_data')
+
 
 class JobData(SourceDataDemo):
 

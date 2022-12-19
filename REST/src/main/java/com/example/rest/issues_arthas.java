3c07648afbeb0a927f3bc17eaa63a30e2cb9121b
Wang Hailong <2905219543@qq.com>
2022-12-19 12:49:36 +0800

    restful

---
+++ REST/src/main/java/com/example/rest/issues_arthas.java
@@ -0,0 +1,31 @@
+package com.example.rest;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+@RestController
+@RequestMapping("alibaba/arthas/issues")
+public class issues_arthas {
+  Scanner in  = new Scanner(new File("issues_arthas.txt"));
+  
+  public issues_arthas() throws FileNotFoundException {
+  }
+  
+  @GetMapping
+  public String get() {
+    ArrayList<String> a = new ArrayList<>();
+    while (in.hasNext()) {
+      a.add(in.next());
+    }
+    String js = JSON.toJSONString(a);
+    return js;
+  }
+}
+
